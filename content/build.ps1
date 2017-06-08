001fb4222b74b5fc7b823eacf0ebbbba7ce4cc64
Bastian Eicher <bastian.eicher@axoom.com>
2017-06-08 14:28:10 +0200

    Tweaked build script

--- content/build.ps1
+++ content/build.ps1
@@ -4,6 +4,7 @@ $SolutionName = "Axoom.MyLibrary"
 
 pushd "$(Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)/src"
 
+dotnet clean "$SolutionName.sln"
 dotnet msbuild /t:Restore /t:Build /p:Configuration=Release /p:Version=$Version "$SolutionName.sln"
 dotnet test --configuration Release --no-build "$SolutionName.UnitTests/$SolutionName.UnitTests.csproj"
 
