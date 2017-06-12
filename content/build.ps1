5a32ce545782adcf9665c124663d5dfed784b717
Bastian Eicher <bastian.eicher@axoom.com>
2017-06-12 19:03:11 +0200

    Tweaked build script

--- content/build.ps1
+++ content/build.ps1
@@ -2,10 +2,12 @@ Param ([Parameter(Mandatory=$True)][string]$Version)
 $ErrorActionPreference = "Stop"
 $SolutionName = "Axoom.MyLibrary"
 
-pushd "$(Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)/src"
+pushd $(Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
+pushd src
 
 dotnet clean "$SolutionName.sln"
 dotnet msbuild /t:Restore /t:Build /p:Configuration=Release /p:Version=$Version "$SolutionName.sln"
 dotnet test --configuration Release --no-build "$SolutionName.UnitTests/$SolutionName.UnitTests.csproj"
 
+popd
 popd
