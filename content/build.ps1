7724d25c05f7e13bba1ac73e86749a5efd170027
Bastian Eicher <bastian.eicher@axoom.com>
2017-07-27 13:53:15 +0200

    Improved build script

--- content/build.ps1
+++ content/build.ps1
@@ -2,6 +2,6 @@ Param ([Parameter(Mandatory=$True)][string]$Version)
 $ErrorActionPreference = "Stop"
 pushd $(Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
 
-.\src\build-dotnet.ps1 $Version
+.\src\build.ps1 $Version
 
 popd
