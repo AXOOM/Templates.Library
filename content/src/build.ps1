632a076ae23746f9661ede47ad7b05373bf4cdf3
Bastian Eicher <bastian.eicher@axoom.com>
2018-04-11 13:11:06 +0200

    Applied EditorConfig

--- content/src/build.ps1
+++ content/src/build.ps1
@@ -1,4 +1,4 @@
-Param ([string]$Version = "0.1-debug")
+﻿Param ([string]$Version = "0.1-debug")
 $ErrorActionPreference = "Stop"
 pushd $(Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
 
