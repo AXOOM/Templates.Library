576a36f38ffc8decd7e17aeac49e27effb83054d
Bastian Eicher <bastian@eicher.net>
2018-04-16 10:27:14 +0200

    Renamed .csproj files

--- content/src/test.ps1
+++ content/src/test.ps1
@@ -2,6 +2,6 @@
 $ErrorActionPreference = "Stop"
 pushd $(Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
 
-dotnet test --configuration Release --no-build Axoom.MyLibrary.UnitTests/Axoom.MyLibrary.UnitTests.csproj
+dotnet test --no-build --configuration Release UnitTests\UnitTests.csproj
 
 popd
